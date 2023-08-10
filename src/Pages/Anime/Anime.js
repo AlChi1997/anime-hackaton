1a95ad74824ffcf9d20ed1f98603927a0a28ef6b
AlChi1997 <alex.chi1997@hotmail.com>
2023-08-10 13:31:13 -0400

    changed styling for anime and searchbar

--- src/Pages/Anime/Anime.js
+++ src/Pages/Anime/Anime.js
@@ -3,6 +3,7 @@ import {useParams} from 'react-router-dom';
 import axios from 'axios';
 import Header from '../../Component/Header/Header';
 import './Anime.scss';
+import { Link } from 'react-router-dom';
 
 const Anime = () => {
 
@@ -12,14 +13,17 @@ const Anime = () => {
     const [anime, setAnime] = useState({});
     const [genres, setGenres] = useState([]);
     const [streamingLinks, setStreamingLinks] = useState([]);
+    const [animeList, setAnimeList] = useState([]);
 
 
     useEffect(() => {
     axios
-    .get(`https://kitsu.io/api/edge/anime?filter[text]=${encodeURIComponent(title)}`)
+    .get(`https://kitsu.io/api/edge/anime?filter[text]=${encodeURIComponent(title)}&page[limit]=5`)
     .then((res) => {;
         setAnime(res.data.data[0]);
+        setAnimeList(res.data.data.slice(1));
         console.log(res.data.data[0])
+        console.log(res.data.data.slice(1));
     })
     .catch((err) => {
         console.log(err);
@@ -53,30 +57,67 @@ const Anime = () => {
     console.log(anime);
 
     return (
-        <div>
-            <section>
-                <Header />
-                <div>
-                    <img style={{width:"100%"}} src={anime.attributes?.coverImage?.original} alt={title}/>
-                </div>
-                <h1>{title}</h1>
-                <p>Average Rating: {anime.attributes?.averageRating}</p>
-                <div>
-                    <h2>Genres:</h2>
-                    {genres.map((genre) => {
-                        return <p key={genre.id}>{genre.attributes.name}</p>
-                    })}
-                </div>
-                <p>{anime.attributes?.synopsis}</p>
-                <div>
-                    {streamingLinks.map((streamingLink) => {
-                        return (
-                            <div key={streamingLink.id}>
-                                <a href={streamingLink.attributes.url}>{streamingLink.attributes.url}</a>
-                            </div>
+        <div className='animepage'>
+            <div className='overlay'></div>
+            <Header />
+            <div className='anime__theme'>
+                    <img className='anime__theme-img' src={anime.attributes?.coverImage?.original} alt={title}/>
+            </div>
+            <section className='anime'>
+                <section className='anime__block'>
+                <section className='anime__info'>
+                    <h1 className='anime__title'>{title}</h1>
+                    <div className='anime__poster'>
+                        <img className='anime__poster-img' src={anime.attributes?.posterImage?.medium} alt={title}/>
+                    </div>
+                </section>
+                <section className='anime__extra'>
+                    <div>
+                        <p className='anime__synopsis'>{anime.attributes?.synopsis}</p>
+                    </div>
+                    <section className='anime__data'>
+                    <div className='anime__additional'>
+                        <h3 className='anime__additional-title'>Details</h3>
+                        <p className='anime__rating'>Average Rating: {anime.attributes?.averageRating}</p>
+                        <p className='anime__showtype'>Showtype: {anime.attributes?.showType}</p>
+                        <p className='anime__episode'>Episodes: {anime.attributes?.episodeCount}</p>
+                        <p className='anime__status'>Status: {anime.attributes?.status}</p>
+                        <p className='anime__aired'>Aired: {anime.attributes?.startDate} - {anime.attributes?.endDate}</p>
+                    </div>
+                    <div className='anime__genre'>
+                        <h3 className='anime__genre-title'>Genres:</h3>
+                        {genres.map((genre) => {
+                            return <p className="anime__genre-name" key={genre.id}>{genre.attributes.name}</p>
+                        })}
+                    </div>
+                    <div className='anime__streaminglinks'>
+                        <h3 className='anime__streaminglinks-title'>Streaming Links</h3>
+                        {streamingLinks.map((streamingLink) => {
+                            return (
+                                <div className='anime__streaminglinks-box' key={streamingLink.id}>
+                                    <a className='anime__streaminglinks-link' href={streamingLink.attributes.url}>{streamingLink.attributes.url}</a>
+                                </div>
+                                )
+                            })
+                        }
+                    </div>
+                    </section>
+                </section>
+                </section>
+                <div className='anime__related'>
+                    <h2 className='anime__related-title'>Related Anime:</h2>
+                    <div className='anime__related-container'>
+                        {animeList.map((anime) => {
+                            return (
+                                <Link className='anime__related-box' to={{pathname: `/anime/${encodeURIComponent(anime.attributes.canonicalTitle)}`}}>
+                                <div key={anime.id}>
+                                    <img className='anime__related-img' src={anime.attributes.posterImage?.tiny} alt={anime.attributes.canonicalTitle}/>
+                                    <p className='anime__related-name'>{anime.attributes.canonicalTitle}</p>
+                                </div>
+                                </Link>
                             )
-                        })
-                    }
+                        })}
+                    </div>
                 </div>
             </section>
         </div>

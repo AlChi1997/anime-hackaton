54649dcaf4f68b7b2d64ea5a908a970db87c85f3
AlChi1997 <alex.chi1997@hotmail.com>
2023-08-10 13:30:59 -0400

    changed styling

--- src/Component/SearchBar/SearchResultsList.jsx
+++ src/Component/SearchBar/SearchResultsList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchResult from './SearchResult';
 import './Search.scss'
 export const SearchResultsList = ({results}) => {
+    
     return (
         <div className="search-results">
             {results.map((result, id) => {
